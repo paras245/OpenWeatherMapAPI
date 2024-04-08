4cc0289d5cbce444d59852c88346ddc90d7cc51b
Paras Panchal <paraspanchal5555@gmail.com>
2024-04-08 22:23:04 +0530

    Add project files.

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using OpenWeatherMapAPI.Models;
+
+namespace OpenWeatherMapAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet("[action]/{city}")]
+        public async Task<IActionResult> City(string city)
+        {
+            // return Ok(new { Temp = "12", Summary = "Barmy", City = city });
+
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri("http://api.openweathermap.org");
+                    //Place Your Open API keey in appid
+                    var response = await client.GetAsync($"/data/2.5/weather?q={city}&appid=************&units=metric");
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    var rawWeather = JsonConvert.DeserializeObject<OpenWeatherResponse>(stringResult);
+                    return Ok(new
+                    {
+                        Temp = rawWeather.Main.Temp,
+                        Summary = string.Join(",", rawWeather.Weather.Select(x => x.Main)),
+                        City = rawWeather.Name
+                    });
+                }
+                catch (HttpRequestException httpRequestException)
+                {
+                    return BadRequest($"Error getting weather from OpenWeather: {httpRequestException.Message}");
+                }
+            }
+        }
+
+    }
+}
